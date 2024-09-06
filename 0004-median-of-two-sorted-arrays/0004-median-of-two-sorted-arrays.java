1521ff5ec383b136e9aa5827fbe1e4b96ff10ff7
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-09-06 12:54:39 +0530

    Time: 1 ms (100.00%), Space: 45.6 MB (91.71%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,40 +1,45 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] mergedArray = new int[m + n];
-        
+        // Merge the two arrays into one sorted array
+        int[] mergedArray = merge(nums1, nums2);
+        int n = mergedArray.length;
+
+        // Find the median
+        if (n % 2 == 0) {
+            // If even, average the two middle elements
+            return (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2.0;
+        } else {
+            // If odd, return the middle element
+            return mergedArray[n / 2];
+        }
+    }
+
+    // Merge two sorted arrays into a single sorted array
+    public int[] merge(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] mergedArray = new int[n1 + n2];
         int i = 0, j = 0, k = 0;
-        
-        // Merge both arrays
-        while (i < m && j < n) {
+
+        // Merge the two arrays
+        while (i < n1 && j < n2) {
             if (nums1[i] < nums2[j]) {
                 mergedArray[k++] = nums1[i++];
             } else {
                 mergedArray[k++] = nums2[j++];
             }
         }
-        
-        // If any elements are left in nums1
-        while (i < m) {
+
+        // Add remaining elements from nums1
+        while (i < n1) {
             mergedArray[k++] = nums1[i++];
         }
-        
-        // If any elements are left in nums2
-        while (j < n) {
+
+        // Add remaining elements from nums2
+        while (j < n2) {
             mergedArray[k++] = nums2[j++];
         }
-        
-        // Find the median
-        int totalLength = m + n;
-        if (totalLength % 2 == 1) {
-            // Odd length, return the middle element
-            return mergedArray[totalLength / 2];
-        } else {
-            // Even length, return the average of the two middle elements
-            int mid1 = totalLength / 2;
-            int mid2 = mid1 - 1;
-            return (mergedArray[mid1] + mergedArray[mid2]) / 2.0;
-        }
+
+        return mergedArray;
     }
 }
