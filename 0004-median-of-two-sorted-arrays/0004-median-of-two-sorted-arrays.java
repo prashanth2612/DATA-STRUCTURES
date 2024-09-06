cafbba49a26258a47e62abb811ae6f0d0147337e
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-09-06 12:56:30 +0530

    Time: 1 ms (100.00%), Space: 45.8 MB (82.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Merge the two arrays into one sorted array
-        int[] mergedArray = merge(nums1, nums2);
-        int n = mergedArray.length;
-
-        // Find the median
-        if (n % 2 == 0) {
-            // If even, average the two middle elements
-            return (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2.0;
-        } else {
-            // If odd, return the middle element
-            return mergedArray[n / 2];
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-    }
 
-    // Merge two sorted arrays into a single sorted array
-    public int[] merge(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int[] mergedArray = new int[n1 + n2];
-        int i = 0, j = 0, k = 0;
+        int low = 0, high = n1;
 
-        // Merge the two arrays
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
-                mergedArray[k++] = nums1[i++];
+        while (low <= high) {
+            int partition1 = (low + high) / 2;
+            int partition2 = (n1 + n2 + 1) / 2 - partition1;
+
+            int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
+            int minRight1 = (partition1 == n1) ? Integer.MAX_VALUE : nums1[partition1];
+
+            int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
+            int minRight2 = (partition2 == n2) ? Integer.MAX_VALUE : nums2[partition2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                // We have partitioned correctly
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return Math.max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 > minRight2) {
+                high = partition1 - 1; // Move towards the left in nums1
             } else {
-                mergedArray[k++] = nums2[j++];
+                low = partition1 + 1; // Move towards the right in nums1
             }
         }
 
-        // Add remaining elements from nums1
-        while (i < n1) {
-            mergedArray[k++] = nums1[i++];
-        }
-
-        // Add remaining elements from nums2
-        while (j < n2) {
-            mergedArray[k++] = nums2[j++];
-        }
-
-        return mergedArray;
+        throw new IllegalArgumentException("Input arrays are not sorted.");
     }
 }
