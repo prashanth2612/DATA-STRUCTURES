71d4c5f2379e124f6e4fbd142659eab56f48cdc0
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-09-09 21:30:44 +0530

    Time: 834 ms (16.97%), Space: 52.9 MB (12.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,25 @@
 class Solution {
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-
-        for(int i=0; i<nums.length; i++) {
-
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-
-            int j = i+1;
-            int k = nums.length-1;
-            while(j<k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum>0) {
-                    k--;
-                }
-                else if (sum < 0){
-                    j++;
-                }
-                else{
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while(nums[j]==nums[j-1] && j<k){
-                        j++;
-                    }
-                }
+public List<List<Integer>> threeSum(int[] nums) {
+    Set<List<Integer>> res = new HashSet<>();
+    if (nums.length == 0) return new ArrayList<>(res); // Convert Set to List
+    Arrays.sort(nums); // Sort the array
+    
+    for (int i = 0; i < nums.length - 2; i++) {
+        int j = i + 1;  // Second pointer
+        int k = nums.length - 1;  // Third pointer
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                // Add the triplet to the Set
+                res.add(Arrays.asList(nums[i], nums[j++], nums[k--])); // Convert array elements to List
+            } else if (sum > 0) {
+                k--; // Decrease right pointer
+            } else {
+                j++; // Increase left pointer
             }
         }
-            return ans;
-   }
+    }
+    return new ArrayList<>(res); // Convert Set to List
+}
 }
